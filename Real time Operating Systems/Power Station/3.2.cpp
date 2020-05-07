469e9a74c277b72c4dfb444f6a6f466c1ba03460
temicch <temicch@gmail.com>
2020-05-08 01:03:09 +0700

    Added OS

---
+++ Real time Operating Systems/Power Station/3.2.cpp
@@ -0,0 +1,128 @@
+#include <vingraph.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/dispatch.h>
+#include <pthread.h>
+#include "warehouse.h"
+#include "transport.h"
+ 
+int WidgetText;
+ 
+char text[100];
+struct arg_struct
+{
+    int arg1, arg2;
+};
+struct msg_transport
+{
+    transport *oTransport;
+    int rcvid;
+};
+ 
+#define ATTACH_POINT "boilerOrder"					// Здесь перед boilerOrder надо добавить HOSTNAME сервера
+#define ATTACH_POINT_TTB "TransportToBroiler"
+ 
+int Channel;
+name_attach_t *Channel_TTB;
+ 
+void *transportLeft(void *args)
+{
+        ((msg_transport*)args)->oTransport->hide();
+        while(((msg_transport*)args)->oTransport->X > -30)
+        {
+            ((msg_transport*)args)->oTransport->move(((msg_transport*)args)->oTransport->X - 1, ((msg_transport*)args)->oTransport->Y);
+            delay(1);
+        }          
+        delete ((msg_transport*)args)->oTransport;
+        MsgReply(((msg_transport*)args)->rcvid, 0, NULL, 0);
+}
+ 
+void *boilerWork(void *args)
+{  
+    warehouse object(((arg_struct *)args)->arg1, ((arg_struct *)args)->arg2, 5, cLightGreen);
+    pthread_t tTransportLeft;
+    msg_transport msg_t;
+    while(1)
+    {
+        object.Remove();
+        if(object.getValue() == 2)
+        {
+            if(MsgSendPulse(Channel, 1, 0,  (object.getX() + 40 - 15)) == -1)          
+            {
+                //sprintf(text, "Error: %d", errno);
+                SetText(WidgetText, "Can't send pulse!");
+                //break;
+            }
+        }
+        if(object.isEmpty())
+        {
+            int receive;
+            //int smsg = (object.getX() + 40 - 15);
+            //if(MsgSend(Channel, &smsg, sizeof(smsg), &receive, sizeof(receive)) == -1)
+            {
+                //sprintf(text, "Error: %d", errno);
+                //SetText(WidgetText, text);
+                //break;
+            }
+            int rcvid = MsgReceive(Channel_TTB->chid, &receive, 1, 0);
+            transport *oTransport = new transport(-35, 50, (receive >> 5));
+            oTransport->show();
+            while(oTransport->X < object.getX() + 40 - 15)
+            {
+                oTransport->move(oTransport->X + 1, oTransport->Y);
+                delay(1);
+            }
+            for(int i = 0; i < (receive & 0x1F); i++)
+            {
+                object.Add();
+                delay(100);
+            }
+            msg_t.rcvid = rcvid;
+            msg_t.oTransport = oTransport;
+            pthread_create(&tTransportLeft,NULL, transportLeft,(void *)&msg_t);
+        }
+        delay(500 + (rand() % 5) * 200);
+    }
+}
+ 
+int main()
+{
+    arg_struct msg;
+    ConnectGraph("client");
+    Fill(RGB(0, 0, 0));
+    EnlargeTo(0, 300, 1520, 400);
+    WidgetText = Text(0, 350, "Empty", RGB(255, 0, 0));
+   
+    while((Channel = name_open(ATTACH_POINT, 0)) == -1)
+    {
+        SetText(WidgetText, "Try to connect...");
+        delay(1000);
+    }
+    Channel_TTB = name_attach(NULL, ATTACH_POINT_TTB, 0);
+    SetText(WidgetText, "Connected");
+    pthread_t tboilerWork[4];
+    for(int i = 0; i < 8; i++)
+    {
+        msg.arg1 = 170 + i * 102;
+        msg.arg2 = 90;
+        pthread_create(&tboilerWork[i],NULL, boilerWork,(void *)&msg);
+    }  
+    while(1)
+    {
+        int c = InputChar();
+        //81 - left
+        //82 - up
+        //83 - right
+        //84 - down
+        if((int)c == 27)
+            break;
+    }
+    for(int i = 0; i < 8; i++)
+    {
+        pthread_cancel(tboilerWork[i]);
+    }
+    name_close(Channel);
+    CloseGraph();
+    _exit(0);
+    return 0;
+}
---
+++ Real time Operating Systems/Power Station/sc.sh
@@ -0,0 +1,3 @@
+cc 3.1.cpp -l vg -l m -o server
+cc 3.2.cpp -l vg -l m -o client
+cc 3.cpp -l vg -l m -o 3
